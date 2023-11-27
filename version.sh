39b28083070896a5eb15c78579047a9d4dc4dea4
Robin <robin@neneos.com>
2023-11-27 12:53:13 +0100

    :muscle: test
    update this

--- version.sh
+++ version.sh
@@ -10,22 +10,28 @@ get_next_version () {
     MINOR=$(echo "$CURRENTTAG" | cut -d'.' -f 2)
     
     PATCH=$(echo "$CURRENTTAG" | cut -d'.' -f 3)
-    
-    if [[ $GITDESCRIPTION = *"patch"* || $GITDESCRIPTION = *":fire:"* || $GITDESCRIPTION = *":bug:"* || $GITDESCRIPTION = *":poop:"* ]]; then
-        PATCH=$(($PATCH + 1))
-        echo "$MAJOR.$MINOR.$PATCH"
-    else
-        if [[ $GITDESCRIPTION = *"minor"* || $GITDESCRIPTION = *":tada:"* ]]; then
-                MINOR=$(($MINOR + 1))
-                echo "$MAJOR.$MINOR.0"  
-        else   
-            if [[ $GITDESCRIPTION = *"major"* || $GITDESCRIPTION = *":boom:"* ]]; then
-                MAJOR=$(($MAJOR + 1))
-                echo "$MAJOR.0.0"
-            else 
-                echo "error"
-                exit 1
-            fi    
+
+    if [[ $GITDESCRIPTION = *"muscle"* ]]; then
+            # check forced version
+            # commit should look like this :muscle: 0.0.0 description
+            echo $GITDESCRIPTION | cut  -c 10-16
+    else    
+        if [[ $GITDESCRIPTION = *"patch"* || $GITDESCRIPTION = *":fire:"* || $GITDESCRIPTION = *":bug:"* || $GITDESCRIPTION = *":poop:"* ]]; then
+            PATCH=$(($PATCH + 1))
+            echo "$MAJOR.$MINOR.$PATCH"
+        else
+            if [[ $GITDESCRIPTION = *"minor"* || $GITDESCRIPTION = *":tada:"* ]]; then
+                    MINOR=$(($MINOR + 1))
+                    echo "$MAJOR.$MINOR.0"  
+            else   
+                if [[ $GITDESCRIPTION = *"major"* || $GITDESCRIPTION = *":boom:"* ]]; then
+                    MAJOR=$(($MAJOR + 1))
+                    echo "$MAJOR.0.0"
+                else 
+                    echo "error"
+                    exit 1
+                fi    
+            fi
         fi
     fi
    
